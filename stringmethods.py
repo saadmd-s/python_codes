0d16b134328f523b6f468fa8188cf8b5582f28e5
mdsaad-s <saadsowcar@gmail.com>
2025-05-29 06:27:53 +0530

    added the string methods

---
+++ stringmethods.py
@@ -0,0 +1,8 @@
+subject="There are many programming languages"
+print(len(subject))
+print(subject.lower())
+print(subject.upper())
+print(subject.find("m"))
+print(subject.replace("T","t"))
+print("many" in subject)
+print("Many" in subject)
